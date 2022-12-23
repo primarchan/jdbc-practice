f9015b692a6f3c21bc91162bec8a7af5f0961bec
An T.G <boltcarrier@naver.com>
2022-12-24 06:47:48 +0900

    JDBC 프로그래밍 실습 - 실습한 JDBC 코드 리팩토링 및 DB 커넥션 풀 적용

--- src/main/java/org/example/ConnectionManager.java
+++ src/main/java/org/example/ConnectionManager.java
@@ -3,16 +3,40 @@ package org.example;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionManager {
+    private static final String DB_DRIVER = "org.h2.Driver";  // 단축키 : command + option + C
+    private static final String DB_URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    public static final String USER_NAME = "sa";
+    public static final String USER_PASSWORD = "";
+    public static final int MAX_POOL_SIZE = 40;
+    private static final DataSource ds;
+
+    static {
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setDriverClassName(DB_DRIVER);
+        hikariDataSource.setJdbcUrl(DB_URL);
+        hikariDataSource.setUsername(USER_NAME);
+        hikariDataSource.setPassword(USER_PASSWORD);
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE);
+        hikariDataSource.setMinimumIdle(MAX_POOL_SIZE);
+
+        ds = hikariDataSource;
+    }
+
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 
     public static DataSource getDataSource() {
-        HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDriverClassName("org.h2.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1");
-        hikariDataSource.setUsername("sa");
-        hikariDataSource.setUsername("");
-
-        return hikariDataSource;
+        return ds;
     }
+
 }
---
+++ src/main/java/org/example/PreparedStatementSetter.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public interface PreparedStatementSetter {
+
+    void setter(PreparedStatement pstmt) throws SQLException;
+
+}
